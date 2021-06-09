cca62c20ba7711ef7d29700be5b9df060c0cc66f
telurio soares <telurio.soares@base2.com.br>
2021-06-09 14:19:17 -0300

    Add project first simple test

---
+++ src/main/java/test/APIRest/App.java
@@ -0,0 +1,22 @@
+package test.APIRest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+
+	public static float ExeCalculo (float v01, float v02) {
+		float Soma = v01 + v02;
+		return Soma;
+	}
+
+
+}
